3ae64433efa58667a808a8f1179207bf85534a62
Robert Mack <robertmack123123@gmail.com>
2024-11-13 11:52:53 -0600

    Finished /tuberorders endpoints

--- TuberTreats/Models/DTOs/TuberOrderDTO.cs
+++ TuberTreats/Models/DTOs/TuberOrderDTO.cs
@@ -2,10 +2,10 @@ public class TuberOrderDTO
 {
     public int Id { get; set; }
     public DateTime OrderPlaceOnDate { get; set; }
-    public DateTime? DeliveredOnDate { get; set; }
+    public DateTime? DeliveredOnDate { get; set; } // Make this nullable to handle undelivered orders
     public int CustomerId { get; set; }
     public CustomerDTO Customer { get; set; }
-    public int? TuberDriverId { get; set; }
+    public int? TuberDriverId { get; set; } // Driver is nullable initially since no driver is assigned
     public TuberDriverDTO TuberDriver { get; set; }
     public List<ToppingDTO> Toppings { get; set; }
 }
